f8ce33b5d2bd1cf9dc2808958e78efd919d708a2
seong9566 <dlgustjd9566@gmail.com>
2022-10-13 19:56:46 +0900

    레포 새로 생성

---
+++ src/test/java/site/metacoding/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
